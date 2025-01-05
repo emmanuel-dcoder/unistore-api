f777a1ef34d4e3b368bc858c817c5e8c9d0655a7
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-01-05 09:35:04 +0100

    adding user

---
+++ src/core/guards/user.guard.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class UserGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+
+    // Check if user_type is 'user'
+    if (request.user.user_type === 'user') {
+      return true;
+    }
+    throw new UnauthorizedException(
+      'Access denied. this route is for user and not merchant.',
+    );
+  }
+}
