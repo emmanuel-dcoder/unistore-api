f9407ab322331c6f87be79bda80d055d9f348144
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-01-10 15:57:55 +0100

    new update

--- src/invoice/service/webhook.service.ts
+++ src/invoice/service/webhook.service.ts
@@ -14,26 +14,45 @@ export class WebhookService {
     if (payload.event === 'charge.completed') {
       if (payload.status === 'successful') {
         await this.handleSuccess(payload);
-      } else {
+      } else if (payload.status === 'failed') {
         await this.handleFailure(payload);
+      } else if (payload.status === 'pending') {
+        await this.handlePending(payload);
       }
     }
   }
 
   private async handleSuccess(payload: any) {
+    console.log('this is for webhook payload', payload);
     const order = await this.orderRepo.findOne({
       where: { reference: payload.meta.authorization.transfer_reference },
     });
-    order.status = 'paid';
-    await this.orderRepo.save(order);
+    if (order) {
+      order.status = 'paid';
+      await this.orderRepo.save(order);
+    }
     return order;
   }
+
   private async handleFailure(payload: any) {
     const order = await this.orderRepo.findOne({
       where: { reference: payload.meta.authorization.transfer_reference },
     });
-    order.status = 'failed';
-    await this.orderRepo.save(order);
+    if (order) {
+      order.status = 'failed';
+      await this.orderRepo.save(order);
+    }
+    return order;
+  }
+
+  private async handlePending(payload: any) {
+    const order = await this.orderRepo.findOne({
+      where: { reference: payload.meta.authorization.transfer_reference },
+    });
+    if (order) {
+      order.status = 'pending'; // You can customize the status or message
+      await this.orderRepo.save(order);
+    }
     return order;
   }
 }
