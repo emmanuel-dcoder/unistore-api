d3c043bed71697bfd85688729ef0e578720c7020
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-01-14 10:26:54 +0100

    new update

---
+++ webhook.ts
@@ -0,0 +1,37 @@
+// Received webhook: {
+//   event: 'charge.completed',
+//   data: {
+//     id: 8321945,
+//     tx_ref: 'order_1736846575470',
+//     flw_ref: '8016915561111736846576262',
+//     device_fingerprint: 'N/A',
+//     amount: 50000,
+//     currency: 'NGN',
+//     charged_amount: 50000,
+//     app_fee: 700,
+//     merchant_fee: 0,
+//     processor_response: 'success',
+//     auth_model: 'AUTH',
+//     ip: '54.75.161.64',
+//     narration: 'Damilola Aguda 1720455877276',
+//     status: 'successful',
+//     payment_type: 'bank_transfer',
+//     created_at: '2025-01-14T09:23:03.000Z',
+//     account_id: 2508309,
+//     customer: {
+//       id: 2571514,
+//       name: 'jo boy',
+//       phone_number: '6756453647664',
+//       email: '****',
+//       created_at: '2025-01-14T09:22:55.000Z'
+//     }
+//   },
+//   meta_data: {
+//     sideNote: 'This is a side note to track this payment request',
+//     originatoraccountnumber: '123*******90',
+//     originatorname: 'JOHN DOE',
+//     bankname: 'Access Bank',
+//     originatoramount: 'N/A'
+//   },
+//   'event.type': 'BANK_TRANSFER_TRANSACTION'
+// };
