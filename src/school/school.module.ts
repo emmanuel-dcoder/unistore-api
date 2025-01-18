64aebac204109cc920fd3ba7c244a39a14e23d41
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-01-18 01:22:50 +0100

    new changes

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { SchoolController } from './school.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { School } from './entities/school.entity';
 import { CloudinaryService } from 'src/core/cloudinary/cloudinary.service';
+import { VerifyTokenMiddleware } from 'src/core/common/middlewares';
 
 @Module({
   imports: [TypeOrmModule.forFeature([School])],
   controllers: [SchoolController],
   providers: [SchoolService, CloudinaryService],
 })
-export class SchoolModule {}
+export class SchoolModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(VerifyTokenMiddleware)
+      .forRoutes(SchoolController)
+      .apply(VerifyTokenMiddleware);
+  }
+}
