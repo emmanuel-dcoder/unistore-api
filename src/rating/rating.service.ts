6e3d66c0ea1b3c87c69a7e45e641de9201a48263
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-01-30 20:23:45 +0100

    rating exception

--- src/rating/rating.service.ts
+++ src/rating/rating.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Rating } from './entities/rating.entity';
 import { Repository } from 'typeorm';
@@ -18,84 +18,98 @@ export class RatingService {
   ) {}
 
   async create(createRatingDto: CreateRatingDto, user: string) {
-    const { product, rating_number, comment } = createRatingDto;
-
-    if (rating_number < 1 || rating_number > 5) {
-      throw new BadRequestException('Rating number must be between 1 and 5');
-    }
-
-    const validate = await this.productService.findById(product);
-    if (!validate) {
-      throw new BadRequestException('Invalid product ID');
-    }
-
-    const rating = this.ratingRepo.create({
-      rating_number,
-      comment,
-      product: { id: product } as any,
-      ratedBy: { id: user } as any,
-    });
-
-    const result = await this.ratingRepo.save(rating);
-    if (!result) {
-      throw new BadRequestException('Unable to create rating');
-    }
-
-    const ratings = await this.ratingRepo.find({
-      where: { product: { id: product } },
-    });
-    const avgRating =
-      ratings.reduce((sum, r) => sum + r.rating_number, 0) / ratings.length;
-
-    await this.productService.updateWithoutImage(product, {
-      avg_rating: avgRating,
-    });
-
     try {
-      await this.mailService.sendMailNotification(
-        validate.user.email,
-        'Product Rating',
-        {
-          name: validate.user.first_name,
-          productName: validate.product_name,
-          ratedBy: rating.ratedBy.first_name,
-          ratings: rating_number,
-        },
-        'rating',
-      );
+      const { product, rating_number, comment } = createRatingDto;
 
-      await this.notificationService.create(
-        {
-          title: 'Product Rating',
-          message: `Hi, your product: ${validate.product_name}  has been rated ${rating_number} by ${rating.ratedBy.first_name}`,
-        },
-        validate.user.id,
-      );
+      if (rating_number < 1 || rating_number > 5) {
+        throw new BadRequestException('Rating number must be between 1 and 5');
+      }
+
+      const validate = await this.productService.findById(product);
+      if (!validate) {
+        throw new BadRequestException('Invalid product ID');
+      }
+
+      const rating = this.ratingRepo.create({
+        rating_number,
+        comment,
+        product: { id: product } as any,
+        ratedBy: { id: user } as any,
+      });
+
+      const result = await this.ratingRepo.save(rating);
+      if (!result) {
+        throw new BadRequestException('Unable to create rating');
+      }
+
+      const ratings = await this.ratingRepo.find({
+        where: { product: { id: product } },
+      });
+      const avgRating =
+        ratings.reduce((sum, r) => sum + r.rating_number, 0) / ratings.length;
+
+      await this.productService.updateWithoutImage(product, {
+        avg_rating: avgRating,
+      });
+
+      try {
+        await this.mailService.sendMailNotification(
+          validate.user.email,
+          'Product Rating',
+          {
+            name: validate.user.first_name,
+            productName: validate.product_name,
+            ratedBy: rating.ratedBy.first_name,
+            ratings: rating_number,
+          },
+          'rating',
+        );
+
+        await this.notificationService.create(
+          {
+            title: 'Product Rating',
+            message: `Hi, your product: ${validate.product_name}  has been rated ${rating_number} by ${rating.ratedBy.first_name}`,
+          },
+          validate.user.id,
+        );
+      } catch (error) {
+        console.log('error:', error);
+      }
+
+      return result;
     } catch (error) {
-      console.log('error:', error);
+      throw new HttpException(
+        error?.response?.message ?? error?.message,
+        error?.status ?? error?.statusCode ?? 500,
+      );
     }
-
-    return result;
   }
 
   async getRatingsByProduct(productId: string) {
-    const ratings = await this.ratingRepo.find({
-      where: { product: { id: productId } },
-      relations: ['ratedBy'], // Populating the ratedBy field
-      select: {
-        ratedBy: {
-          id: true,
-          first_name: true,
-          last_name: true,
-          profile_picture: true,
+    try {
+      const ratings = await this.ratingRepo.find({
+        where: { product: { id: productId } },
+        relations: ['ratedBy'], // Populating the ratedBy field
+        select: {
+          ratedBy: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            profile_picture: true,
+          },
         },
-      },
-    });
+      });
 
-    if (!ratings || ratings.length === 0) {
-      throw new BadRequestException('No ratings found for this product');
+      if (!ratings || ratings.length === 0) {
+        throw new BadRequestException('No ratings found for this product');
+      }
+
+      return ratings;
+    } catch (error) {
+      throw new HttpException(
+        error?.response?.message ?? error?.message,
+        error?.status ?? error?.statusCode ?? 500,
+      );
     }
-
-    return ratings;
   }
 }
