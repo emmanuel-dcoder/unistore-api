1caf6715dfc36b0a8df7498edf0aa7543af410f2
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-01-30 19:47:29 +0100

    changes

--- src/admin/services/admin-invoice.service.ts
+++ src/admin/services/admin-invoice.service.ts
@@ -16,46 +16,46 @@ export class AdminInvoiceService {
     page: number = 1,
     limit: number = 10,
   ): Promise<Invoice[]> {
-    const skip = (page - 1) * limit;
-
-    const queryBuilder = this.invoiceRepo
-      .createQueryBuilder('invoice')
-      .leftJoinAndSelect('invoice.product_owner', 'productOwner');
-
-    if (searchQuery) {
-      queryBuilder
-        .where('invoice.invoice_id LIKE :searchQuery', {
-          searchQuery: `%${searchQuery}%`,
-        })
-        .orWhere('productOwner.first_name LIKE :searchQuery', {
-          searchQuery: `%${searchQuery}%`,
-        })
-        .orWhere('productOwner.last_name LIKE :searchQuery', {
-          searchQuery: `%${searchQuery}%`,
-        })
-        .orWhere('invoice.status LIKE :searchQuery', {
-          searchQuery: `%${searchQuery}%`,
-        });
-    }
-
-    if (status) {
-      queryBuilder.andWhere('invoice.status = :status', { status });
-    }
-
-    queryBuilder.addSelect([
-      'productOwner.id',
-      'productOwner.first_name',
-      'productOwner.last_name',
-      'productOwner.profile_picture',
-      'productOwner.email',
-      'productOwner.phone',
-    ]);
-
-    queryBuilder.skip(skip).take(limit).orderBy('invoice.created_at', 'DESC');
-
-    const invoice = await queryBuilder.getMany();
-    return invoice;
     try {
+      const skip = (page - 1) * limit;
+
+      const queryBuilder = this.invoiceRepo
+        .createQueryBuilder('invoice')
+        .leftJoinAndSelect('invoice.product_owner', 'productOwner');
+
+      if (searchQuery) {
+        queryBuilder
+          .where('invoice.invoice_id LIKE :searchQuery', {
+            searchQuery: `%${searchQuery}%`,
+          })
+          .orWhere('productOwner.first_name LIKE :searchQuery', {
+            searchQuery: `%${searchQuery}%`,
+          })
+          .orWhere('productOwner.last_name LIKE :searchQuery', {
+            searchQuery: `%${searchQuery}%`,
+          })
+          .orWhere('invoice.status LIKE :searchQuery', {
+            searchQuery: `%${searchQuery}%`,
+          });
+      }
+
+      if (status) {
+        queryBuilder.andWhere('invoice.status = :status', { status });
+      }
+
+      queryBuilder.addSelect([
+        'productOwner.id',
+        'productOwner.first_name',
+        'productOwner.last_name',
+        'productOwner.profile_picture',
+        'productOwner.email',
+        'productOwner.phone',
+      ]);
+
+      queryBuilder.skip(skip).take(limit).orderBy('invoice.created_at', 'DESC');
+
+      const invoice = await queryBuilder.getMany();
+      return invoice;
     } catch (error) {
       throw new HttpException(
         error?.response?.message ?? error?.message,
