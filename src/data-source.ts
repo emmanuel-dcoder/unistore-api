fb23b008b248d9b451a913189e34e11e4ce13461
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-02-11 19:46:59 +0100

    data-source

---
+++ src/data-source.ts
@@ -0,0 +1,39 @@
+import { DataSource } from 'typeorm';
+import {
+  POSTGRES_DATABASE,
+  POSTGRES_HOST,
+  POSTGRES_PASSWORD,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+} from './config/keys';
+
+require('dotenv').config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: POSTGRES_HOST,
+  port: parseInt(POSTGRES_PORT),
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
+  synchronize: false, // Should be false for migrations
+  entities: ['dist/**/*.entity.js'],
+  migrationsTableName: 'migration',
+  migrations: ['dist/migrations/*.js'],
+  migrationsRun: false, // Run manually using CLI
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+});
+
+// Initialize the data source (for CLI commands)
+AppDataSource.initialize()
+  .then(() => console.log('Data Source has been initialized!'))
+  .catch((err) => console.error('Error initializing Data Source:', err));
+
+// Ensure that the data source is initialized before running migrations
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
